564a7ef0e3ac5c8f77d0ce944468728ed906a6bc
chon-sakata <skt97yuma@outlook.jp>
2019-10-19 19:40:01 +0900

    ラズパイから予定が聞かれたら答えられるようにした

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,68 +1,7 @@
 const functions = require('firebase-functions');
-const fs = require('fs')
-const readline = require('readline');
+
 const { google } = require('googleapis');
 
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-const TOKEN_PATH = 'token.json';
-
-// const content = fs.readFile('credentials.json', (err, content) => {
-//     if (err) return console.log('Error loading client secret file:', err);
-
-//     return content;
-// });
-
-// const credentials = JSON.parse(content);
-// const { client_secret, client_id, redirect_uris } = credentials.installed;
-// const oAuth2Client = new google.auth.OAuth2(
-//     client_id, client_secret, redirect_uris[0]
-// );
-
-// oAuth2Client.setCredentials({
-//     refresh_token: "ya29.Il-iB18dlym9fMJhCYI_rFMG564H3PNkFNDIMmC-zneSLrpPuB5byU2ZALbucr2t1ZEAv_P434Xc9jMI31dtubHBUYJSDoYI1bNbiUgfeCedeVNJoXWVGw-22RJYAdjx0Q", "refresh_token": "1//0eu77X0wcThNfCgYIARAAGA4SNwF-L9Irl4HBYxi3Tl6-XSwsFhcChpB0gwsdBv_emUduNniuy5nV1cP7MW6wSf1FwkHeMw07BhQ",
-// });
-
-// county = listEvents(oAuth2Client);
-
-function authorize(credentials, callback) {
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]
-    );
-
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getAccessToken(oAuth2Client, callback);
-        //oAuth2Client.setCredentials(JSON.parse(token));
-        // oAuth2Client.setCredentials({
-        //     refresh_token: "ya29.Il-iB18dlym9fMJhCYI_rFMG564H3PNkFNDIMmC-zneSLrpPuB5byU2ZALbucr2t1ZEAv_P434Xc9jMI31dtubHBUYJSDoYI1bNbiUgfeCedeVNJoXWVGw-22RJYAdjx0Q", "refresh_token": "1//0eu77X0wcThNfCgYIARAAGA4SNwF-L9Irl4HBYxi3Tl6-XSwsFhcChpB0gwsdBv_emUduNniuy5nV1cP7MW6wSf1FwkHeMw07BhQ",
-        // });
-        callback(oAuth2Client);
-    });
-}
-
-function getAccessToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error retrieving access token', err);
-            oAuth2Client.setCredentials(token);
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
-}
 
 function listEvents(auth, response) {
     const calendar = google.calendar({ version: 'v3', auth });
@@ -75,20 +14,29 @@ function listEvents(auth, response) {
         maxResults: 10,
         singleEvents: true,
         orderBy: 'startTime',
+        timeZone: "Asia/Tokyo"
     }, (err, res) => {
         if (err) return console.log('The API returned and error: ' + err);
         const events = res.data.items;
-        if (events.length) {
-            console.log('Upcoming 10 events:');
+
+        if (events.length !== 0 && events[0].start.dateTime !== undefined) {
             event = events[0];
-            const start = event.start.dateTime || event.start.date;
+            console.log(event);
+            const start = event.start.dateTime;
+            const parsetime = new Date(start);
+            console.log(typeof (parsetime));
+            const year = parsetime.getFullYear();
+            const month = ('0' + (parsetime.getMonth() + 1)).slice(-2);
+            const day = ('0' + parsetime.getDate()).slice(-2);
+            const hour = ('0' + parsetime.getHours()).slice(-2);
+            const minute = ('0' + parsetime.getMinutes()).slice(-2);
+            const fmttime = year + month + day + hour + minute;
             console.log(`${start} - ${event.summary}`);
-            response.send(`${start} - ${event.summary}`);
+            response.send(`${fmttime}`);
         } else {
             console.log('No upcoming events found.');
-            response.send('No upcoming events found.');
+            response.send('nothing');
         }
-        response.send("Hello from Firebase!" + county);
     });
 
 
@@ -98,7 +46,7 @@ function listEvents(auth, response) {
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.getSchedule = functions.https.onRequest((request, response) => {
     // const content = fs.readFile('credentials.json', (err, content) => {
     //     if (err) return console.log('Error loading client secret file:', err);
 
@@ -112,11 +60,17 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
         client_id, client_secret, redirect_uris[0]
     );
 
+    let refresh_token = ""
+    if (request.query.token !== undefined) {
+        refresh_token = request.query.token
+    } else {
+        //refresh_token = functions.config().calendar.token
+        response.send("token not found");
+    }
+
     oAuth2Client.setCredentials({
-        refresh_token: functions.config().calendar.token,
+        refresh_token: refresh_token,
     });
 
     listEvents(oAuth2Client, response);
-
-    // response.send("Hello from Firebase!" + county);
 });
